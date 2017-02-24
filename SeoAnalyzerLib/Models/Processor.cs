719f06f29dfb6f69be3ffd949fece987532c5490
timreg <timsdp@yandex.ua>
2017-02-24 02:37:31 +0200

    Simple test

--- SeoAnalyzerLib/Models/Processor.cs
+++ SeoAnalyzerLib/Models/Processor.cs
@@ -9,14 +9,19 @@ namespace SeoAnalyzerLib.Models
 {
     public class Processor : IProcessor
     {
+        private Setup options;
         public Report DoProcess()
         {
-            throw new NotImplementedException();
+            return new Report();
         }
 
         public bool Init(Setup options)
         {
-            throw new NotImplementedException();
+            bool retVal = false;
+            this.options = options;
+
+
+            return retVal;
         }
     }
 }
---
+++ SeoAnalyzerTest/TestProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeoAnalyzerLib.Models;
+using System.Collections.Generic;
+
+namespace SeoAnalyzerTest
+{
+    [TestClass]
+    public class TestProcessor
+    {
+        private Setup Options;
+        [TestInitialize]
+        public void Setup()
+        {
+            Options = new Setup()
+            {
+                StopWords = new List<string>() { "and", "or", "a", "the" },
+                OptFilterStopWords = true,
+                OptCalcOccurAll = true,
+                OptCalcOccurMeta = true,
+                OptCalcOccurExtLinks = true
+            };
+        }
+
+
+        [TestMethod]
+        public void TestDoProcess()
+        {
+            Options.SourceString = "asdf sdaf sdf sdf dsf sdfsf";
+            Processor prc = new Processor();
+            prc.Init(this.Options);
+
+            Report report = prc.DoProcess();
+
+            Assert.IsNotNull(report);
+        }
+    }
+}
